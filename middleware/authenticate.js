38d2bb02f01f6eb0f51292965c56e0ff742386a2
Aftab69 <aftabhssn3@gmail.com>
2021-10-01 22:35:30 +0530

    hi

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -2,6 +2,7 @@ const jwt = require("jsonwebtoken");
 const User = require("../model/userSchema");
 
 const Authenticate = async (req,res,next) =>{
+    console.log("hi");
      try{
         const token = req.cookies.jwtoken;
         console.log("token");
